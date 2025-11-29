0956dbedf9dc46f0d2cbd36558886a2225074869
bharathdayal <bharathdayals@gmail.com>
2025-11-29 20:56:04 +0530

    Enhance PaymentResponse with new fields
    Added additional fields to PaymentResponse for order details and transaction info.

--- src/main/java/com/example/payment_process/dto/PaymentResponse.java
+++ src/main/java/com/example/payment_process/dto/PaymentResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,4 +17,18 @@ public class PaymentResponse {
     private String paymentId;
     private String clientSecret;
     private String  message;
+    private Long orderId;
+    private String productDesc;
+    private Long amount;
+    private String currency;
+    private String status;
+    private String paymentUuid;
+
+    // latest transaction info
+    private String lastTransactionUuid;
+    private String lastTransactionStatus;
+    private String lastGateway;
+    private String lastGatewayTransactionId;
+    private Instant lastTransactionCreatedAt;
+
 }
