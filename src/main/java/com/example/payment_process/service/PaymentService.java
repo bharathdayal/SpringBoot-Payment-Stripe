382ad8780bd17f7079582c318062b10bb9e571b3
bharathdayal <bharathdayals@gmail.com>
2025-11-29 20:59:23 +0530

    Add webhook handler and payment status methods

--- src/main/java/com/example/payment_process/service/PaymentService.java
+++ src/main/java/com/example/payment_process/service/PaymentService.java
@@ -2,11 +2,16 @@ package com.example.payment_process.service;
 
 import com.example.payment_process.dto.OrderRequest;
 import com.example.payment_process.dto.PaymentResponse;
-import com.example.payment_process.repository.PaymentRepository;
-import org.aspectj.weaver.ast.Or;
-import org.springframework.core.annotation.Order;
+import com.example.payment_process.model.Payment;
+
 
 public interface PaymentService {
     PaymentResponse createPayment(OrderRequest request,String idempotencyKey);
     String createCheckoutSession(OrderRequest request,String idempotencyKey);
+    String handleWebhook(String sigHeader, String payload);
+    PaymentResponse getPaymentStatus(String uuId);
+    PaymentResponse getPaymentStatus(Long id);
+
+
+
 }
