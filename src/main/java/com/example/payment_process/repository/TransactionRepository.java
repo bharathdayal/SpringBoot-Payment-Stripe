ddd349157684dcab624b1acd67132a25859b1c86
bharathdayal <bharathdayals@gmail.com>
2025-11-29 20:57:26 +0530

    Add methods to TransactionRepository for payments

--- src/main/java/com/example/payment_process/repository/TransactionRepository.java
+++ src/main/java/com/example/payment_process/repository/TransactionRepository.java
@@ -1,13 +1,17 @@
 package com.example.payment_process.repository;
 
+import com.example.payment_process.model.Payment;
 import com.example.payment_process.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
     Optional<Transaction> findByUuid(String uuid);
+    Optional<Transaction> findByGatewayTransactionId(String gatewayTransactionId);
+    List<Transaction> findByPaymentOrderByCreatedAtDesc(Payment payment);
 }
