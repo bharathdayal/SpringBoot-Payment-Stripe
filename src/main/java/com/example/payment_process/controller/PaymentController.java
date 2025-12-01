08c52d5a67ba99c178857f5095d424a22748f0c4
bharathdayal <bharathdayals@gmail.com>
2025-12-01 15:46:05 +0530

    Add payment listing endpoint and update checkout method
    Added a new endpoint to list all payments and modified the createCheckout method to include the frontend base URL.

--- src/main/java/com/example/payment_process/controller/PaymentController.java
+++ src/main/java/com/example/payment_process/controller/PaymentController.java
@@ -1,7 +1,9 @@
 package com.example.payment_process.controller;
 
+import com.example.payment_process.component.ResolveFrontendBaseUrl;
 import com.example.payment_process.dto.OrderRequest;
 import com.example.payment_process.dto.PaymentResponse;
+import com.example.payment_process.dto.PaymentSummary;
 import com.example.payment_process.model.Payment;
 import com.example.payment_process.model.Transaction;
 import com.example.payment_process.repository.PaymentRepository;
@@ -15,6 +17,7 @@ import com.stripe.model.StripeObject;
 import com.stripe.model.checkout.Session;
 import com.stripe.net.ApiResource;
 import com.stripe.net.Webhook;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -35,11 +39,14 @@ public class PaymentController {
     private final PaymentService stripeService;
     private final PaymentRepository paymentRepository;
     private final TransactionRepository transactionRepository;
+    private final ResolveFrontendBaseUrl resolveFrontendBaseUrl;
 
-    public PaymentController(@Qualifier("idempotentStripeService") PaymentService stripeService, PaymentRepository paymentRepository, TransactionRepository transactionRepository) {
+    public PaymentController(@Qualifier("idempotentStripeService") PaymentService stripeService, PaymentRepository paymentRepository,
+                             TransactionRepository transactionRepository,ResolveFrontendBaseUrl resolveFrontendBaseUrl) {
         this.stripeService = stripeService;
         this.paymentRepository = paymentRepository;
         this.transactionRepository = transactionRepository;
+        this.resolveFrontendBaseUrl=resolveFrontendBaseUrl;
     }
 
     @PostMapping("/create")
@@ -53,9 +60,12 @@ public class PaymentController {
     @PostMapping("/stripe/checkout")
     public ResponseEntity<Map<String, String>> createCheckout(
             @RequestBody OrderRequest orderRequest,
-            @RequestHeader(value="Idempotency-Key",required = false) String idempotencyKey) throws StripeException {
+            @RequestHeader(value="Idempotency-Key",required = false) String idempotencyKey,
+            HttpServletRequest request) throws StripeException {
 
-        String checkoutUrl = stripeService.createCheckoutSession(orderRequest,idempotencyKey);
+        String baseUrl = resolveFrontendBaseUrl.frontendBaseUrl(request);
+
+        String checkoutUrl = stripeService.createCheckoutSession(orderRequest,idempotencyKey,baseUrl);
         return ResponseEntity.ok(Map.of("checkoutUrl", checkoutUrl,
                 "message", "Stripe Checkout session created successfully"));
     }
@@ -65,9 +75,11 @@ public class PaymentController {
             @RequestHeader("Stripe-Signature") String sigHeader,
             @RequestBody String payload) {
 
-       
+
         stripeService.handleWebhook(sigHeader,payload);
 
+
+        // You can also handle "payment_intent.succeeded" here if needed
 
         return ResponseEntity.ok("");
     }
@@ -76,5 +88,10 @@ public class PaymentController {
     public ResponseEntity<PaymentResponse>getPaymentStatus(@PathVariable String paymentUuid) {
         PaymentResponse response =stripeService.getPaymentStatus(paymentUuid);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<PaymentSummary>>listAllPayments() {
+        return ResponseEntity.ok(stripeService.listAllPayments());
     }
 }
